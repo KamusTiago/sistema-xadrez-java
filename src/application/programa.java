ce0727b2810a5de3792660702369f28ed155f084
KamusTiago <tiagokamus40@gmail.com>
2021-01-21 19:43:57 -0300

    Iniciando tabuleiros e pecas

--- src/application/programa.java
+++ src/application/programa.java
@@ -1,15 +1,13 @@
 package application;
 
 import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Posicao posi = new Posicao(3, 5);
-		
-		System.out.println(posi);
-		
+	Tabuleiro tabu = new Tabuleiro(8, 8);
 		
 	}
 
