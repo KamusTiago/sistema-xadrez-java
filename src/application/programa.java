8a09106134832d885bf814b7de0477df562be970
KamusTiago <tiagokamus40@gmail.com>
2021-01-21 19:32:48 -0300

    primeira classe posicao

--- src/application/programa.java
+++ src/application/programa.java
@@ -1,10 +1,16 @@
 package application;
 
+import tabuleiro.Posicao;
+
 public class programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Olá mundo");
+		Posicao posi = new Posicao(3, 5);
+		
+		System.out.println(posi);
+		
+		
 	}
 
 }
